8ff276ba81d78c8c8e58ec1f18554c0439309aa3
RanaAbdelaziz <39296656+RanaAbdelaziz@users.noreply.github.com>
2023-09-26 18:18:15 +0200

    layout

--- E-Commerce/src/app/core/core.module.ts
+++ E-Commerce/src/app/core/core.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CoreRoutingModule } from './core-routing.module';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MainLayoutComponent } from './components/main-layout/main-layout.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NavbarComponent,
+    MainLayoutComponent
+  ],
   imports: [
     CommonModule,
     CoreRoutingModule
+  ],
+  exports:[
   ]
 })
 export class CoreModule { }
