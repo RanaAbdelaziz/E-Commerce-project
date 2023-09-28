dfb22c485d0d732ac51e6505c4938225b35f1c8a
RanaAbdelaziz <39296656+RanaAbdelaziz@users.noreply.github.com>
2023-09-28 02:34:29 +0200

    order pagination

--- E-Commerce/src/app/Feature/orders/orders-routing.module.ts
+++ E-Commerce/src/app/Feature/orders/orders-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { OrderlistComponent } from './orderlist/orderlist.component';
+import { OrderdetailsComponent } from './orderdetails/orderdetails.component';
 
 const routes: Routes = [
   {
     path:'',
     component:OrderlistComponent
+  },
+  {
+    path: 'order-Details',
+    component: OrderdetailsComponent
   }
 ];
 
--- E-Commerce/src/app/Feature/orders/orders.module.ts
+++ E-Commerce/src/app/Feature/orders/orders.module.ts
@@ -6,11 +6,13 @@ import { OrderlistComponent } from './orderlist/orderlist.component';
 import { OrdersService } from './service/orders.service';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { HttpClientModule } from '@angular/common/http';
+import { OrderdetailsComponent } from './orderdetails/orderdetails.component';
 
 
 @NgModule({
   declarations: [
-    OrderlistComponent
+    OrderlistComponent,
+    OrderdetailsComponent
   ],
   imports: [
     CommonModule,
